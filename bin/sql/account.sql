c48df6c02a6b0ebba090ed01807e73aa220d40da
joepichardo <joepichar@gmail.com>
2020-07-07 00:32:32 -0700

    add live and staging theme ids

--- bin/sql/account.sql
+++ bin/sql/account.sql
@@ -1,5 +1,9 @@
 CREATE TABLE account(
-    id             SERIAL PRIMARY KEY,
-    "storeAddress" TEXT NOT NULL,
-    "accessToken"  TEXT NOT NULL
+    id              SERIAL PRIMARY KEY,
+    "storeAddress"  TEXT NOT NULL,
+    "accessToken"   TEXT NOT NULL,
+    "liveThemeId"     BIGINT NOT NULL,
+    "stagingThemeId"  BIGINT NOT NULL,
+    FOREIGN KEY ("liveThemeId") REFERENCES theme(id),
+    FOREIGN KEY ("stagingThemeId") REFERENCES theme(id)
 );
