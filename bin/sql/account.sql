8c3dcd27fe9a3e096c22dedf3c69b58954534ca3
joepichardo <joepichar@gmail.com>
2020-07-07 01:06:19 -0700

    remove theme ids causing circular references

--- bin/sql/account.sql
+++ bin/sql/account.sql
@@ -1,9 +1,5 @@
 CREATE TABLE account(
     id              SERIAL PRIMARY KEY,
     "storeAddress"  TEXT NOT NULL,
-    "accessToken"   TEXT NOT NULL,
-    "liveThemeId"     BIGINT NOT NULL,
-    "stagingThemeId"  BIGINT NOT NULL,
-    FOREIGN KEY ("liveThemeId") REFERENCES theme(id),
-    FOREIGN KEY ("stagingThemeId") REFERENCES theme(id)
+    "accessToken"   TEXT NOT NULL
 );
