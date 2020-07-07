61ab511269da03b09ae065b260c9b3a47f7f2856
joepichardo <joepichar@gmail.com>
2020-07-07 00:33:50 -0700

    make ownerId bigint and add themeId referencing theme table

--- bin/sql/themeSchedule.sql
+++ bin/sql/themeSchedule.sql
@@ -3,10 +3,12 @@ CREATE TABLE themeSchedule(
     "scheduleAt"    TIMESTAMP WITH TIME ZONE NOT NULL,
     "fileKey"       TEXT NOT NULL,
     "fileValue"     TEXT NOT NULL,
-    "ownerId"       INTEGER NOT NULL,
+    "ownerId"       BIGINT NOT NULL,
     "backupId"      BIGINT NOT NULL,
     description     TEXT NOT NULL,
     deployed        BOOLEAN NOT NULL DEFAULT FALSE,
+    "themeId"      BIGINT NOT NULL,
     FOREIGN KEY ("ownerId") REFERENCES account(id) ON DELETE CASCADE,
-    FOREIGN KEY ("backupId") REFERENCES themeBackup(id)
+    FOREIGN KEY ("backupId") REFERENCES themeBackup(id),
+    FOREIGN KEY ("themeId") REFERENCES theme(id)
 );
