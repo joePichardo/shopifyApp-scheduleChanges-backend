75b9d191542ea229f0f4a38ea0a66c46b829035b
joepichardo <joepichar@gmail.com>
2020-07-07 18:15:51 -0700

    add required routes

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const accountRouter = require('./api/account');
 const gameRouter = require('./api/game');
 const themeRouter = require('./api/theme');
+const AccountTable = require('./account/table');
 
 const app = express();
 
@@ -17,6 +18,25 @@ app.use(cookieParser());
 app.use('/account', accountRouter);
 app.use('/theme', themeRouter);
 // app.use('/game', gameRouter);
+
+app.post('/customers/redact', function (req, res) {
+  res.status(200)
+})
+
+app.post('/customers/data_request', function (req, res) {
+  res.status(200)
+})
+
+app.post('/shop/redact', function (req, res) {
+  const {shop_id, shop_domain} = req.body;
+
+  AccountTable.deleteAccount({ shop_domain })
+    .then(() => {
+      return res.status(200);
+    })
+    .catch(error => next(error));
+
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
