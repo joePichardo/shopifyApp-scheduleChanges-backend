e8ced3f1516a533bbe46602282a89519b8d955ea
joepichardo <joepichar@gmail.com>
2020-07-07 22:02:14 -0700

    add email column

--- bin/sql/account.sql
+++ bin/sql/account.sql
@@ -2,5 +2,6 @@ CREATE TABLE account(
     id                  SERIAL PRIMARY KEY,
     "storeAddress"      TEXT NOT NULL,
     "accessToken"       TEXT NOT NULL,
-    "stagingThemeName"  TEXT DEFAULT '' NOT NULL
+    "stagingThemeName"  TEXT DEFAULT '' NOT NULL,
+    email               TEXT DEFAULT '' NOT NULL
 );
