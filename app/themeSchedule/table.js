73321aa6887dcff07fb0482268aaccfc812ad2cc
joepichardo <joepichar@gmail.com>
2020-07-10 00:33:06 -0700

    fix select query

--- app/themeSchedule/table.js
+++ app/themeSchedule/table.js
@@ -82,12 +82,7 @@ class ThemeScheduleTable {
 
     return new Promise((resolve, reject) => {
       pool.query(
-        'SELECT ts.id AS "scheduleId", ts."scheduleAt", ts."fileKey", ts."fileValue", ts."ownerId" AS "accountId", ts."backupId", ts.description, ac."storeAddress", ac."accessToken"' +
-        'FROM themeschedule ts' +
-        'FULL OUTER JOIN account ac' +
-        'ON ts."ownerId" = ac.id' +
-        'WHERE ts."scheduleAt" <= NOW()' +
-        'AND ts.deployed = FALSE;',
+        'SELECT ts.id AS "scheduleId", ts."scheduleAt", ts."fileKey", ts."fileValue", ts."ownerId" AS "accountId", ts."backupId", ts.description, ac."storeAddress", ac."accessToken" FROM themeschedule ts FULL OUTER JOIN account ac ON ts."ownerId" = ac.id WHERE ts."scheduleAt" <= NOW() AND ts.deployed = FALSE',
         [],
         (error, response) => {
           if (error) {
